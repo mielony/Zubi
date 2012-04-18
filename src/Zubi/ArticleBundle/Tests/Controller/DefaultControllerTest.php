19a26adb59ab452b135aa8e3b930c5b453d5fdd8
mielony <mielniczuk.mariusz@gmail.com>
2012-04-18 21:33:34 +0200

    Testy usuwania artykulow ZROBIONE

--- src/Zubi/ArticleBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Zubi/ArticleBundle/Tests/Controller/DefaultControllerTest.php
@@ -35,4 +35,59 @@ class DefaultControllerTest extends WebTestCase
         $this->assertTrue($crawler->filter('a:contains("do listy")')->count() > 0);    
        // $link = $crawler->filter('a:contains("Greet")')->eq(1)->link();
     }      
+    
+    public function testDeleteArticleGoodUserWrongId()
+    {       
+         $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'b@b.pl',
+            'PHP_AUTH_PW'   => '1234'
+            ));
+        $crawler = $client->request('GET', '/article/delete/0');
+        $this->assertTrue(true);          
+        $this->assertFalse($client->getResponse()->getStatusCode() == '200' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 200!!!!");       
+        $this->assertTrue($client->getResponse()->getStatusCode() == '302' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 302!!!!");       
+    }
+    
+    public function testDeleteArticleWrongUser()
+    {       
+         $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'b@b.pl',
+            'PHP_AUTH_PW'   => '123s'
+            ));
+        $crawler = $client->request('GET', '/article/delete/0');
+        $this->assertTrue(true);          
+        $this->assertFalse($client->getResponse()->getStatusCode() == '200' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 200!!!!");       
+        $this->assertTrue($client->getResponse()->getStatusCode() == '302' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 302!!!!");       
+    }
+    
+    //musi być artykuł o id = 1 
+    public function testDeleteArticleGoodUserGoodId()
+    {       
+         $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'b@b.pl',
+            'PHP_AUTH_PW'   => '1234'
+            ));
+        $crawler = $client->request('GET', '/article/delete/1');
+        $this->assertTrue(true);          
+        $this->assertFalse($client->getResponse()->getStatusCode() == '200' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 200!!!!");       
+        $this->assertTrue($client->getResponse()->getStatusCode() == '302' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 302!!!!");       
+        $cont = $client->getResponse()->getContent();                        
+        $crawler = $client->request('GET', $cont);
+        //jeśli po przekierowaniu w html jest tekst Sukes oznacza to że został
+        // wyświetlony komunikat o sukcesie kasowania
+        $this->assertTrue($crawler->filter('html:contains("Sukces")')->count() > 0);                
+    }
+            
 }
