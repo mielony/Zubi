1c33eb84c5d74c45cd2e5e01359c94b6e1939997
mielony <mielniczuk.mariusz@gmail.com>
2012-04-18 20:47:58 +0200

    Dodane testy EDYCJI FAQ - DZIALA LOGWANIE ADMINA W TESACH

--- src/Zubi/FaqBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Zubi/FaqBundle/Tests/Controller/DefaultControllerTest.php
@@ -21,24 +21,17 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/faq/');    
-        // jeśli jest wyrażenie 1. to znaczy że wyświetliło się przynajmniej jedno 
-        // pytanie
         $this->assertRegExp('/1./', $client->getResponse()->getContent());      
-    //  $this->assertTrue($crawler->filter('html:contains("FAQ")')->count() > 0);   
+ 
     }
     
     public function testFaqDelete()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/faq/delete/2');    
-        // jeśli jest wyrażenie 1. to znaczy że wyświetliło się przynajmniej jedno 
-        // pytanie
-         $this->assertTrue($client->getResponse()->getStatusCode() == '302' ,
+        $this->assertTrue($client->getResponse()->getStatusCode() == '302' ,
                 "Response code is: ".$client->getResponse()->getStatusCode().
-                "not 302!!!!");
-        
-        
-    //  $this->assertTrue($crawler->filter('html:contains("FAQ")')->count() > 0);   
+                "not 302!!!!");          
     }
     
     
@@ -46,13 +39,57 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/faq/edit/2');    
-        // jeśli jest wyrażenie 1. to znaczy że wyświetliło się przynajmniej jedno 
-        // pytanie
-         $this->assertTrue($client->getResponse()->getStatusCode() == '302' ,
+        $this->assertTrue($client->getResponse()->getStatusCode() == '302' ,
                 "Response code is: ".$client->getResponse()->getStatusCode().
                 "not 302!!!!");   
-        
-    //  $this->assertTrue($crawler->filter('html:contains("FAQ")')->count() > 0);   
     }
 
+    public function testFaqEditByAdmin()
+    {
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'b@b.pl',
+            'PHP_AUTH_PW'   => '1234'
+            ));
+        //Test zakłada, że istnieje w bazie FAQ o id = 1;
+        $crawler = $client->request('GET', '/faq/edit/1');    
+        $this->assertTrue($client->getResponse()->getStatusCode() == '200' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 200!!!!");   
+        //sprawdzam czy jest słowo EDYCJA na stronie - projekt zakłada że jest.
+        $this->assertTrue($crawler->filter('html:contains("EDYCJA")')->count() > 0); 
+    }
+    
+    public function testFaqEditByAdminNothingToEdit()
+    {
+          $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'b@b.pl',
+            'PHP_AUTH_PW'   => '123'
+            ));
+        //Test zakłada, że nie istnieje w bazie FAQ o id = 0;
+        $crawler = $client->request('GET', '/faq/edit/0');    
+        $this->assertFalse($client->getResponse()->getStatusCode() == '200' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 302!!!!");   
+        $this->assertTrue($client->getResponse()->getStatusCode() == '302' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 302!!!!");   
+    }
+    
+    public function testFaqEditByAdminWrongPass()
+    {
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'b@b.pl',
+            'PHP_AUTH_PW'   => '123'
+            ));
+        //Test zakłada, że istnieje w bazie FAQ o id = 1;
+        $crawler = $client->request('GET', '/faq/edit/1');    
+        $this->assertFalse($client->getResponse()->getStatusCode() == '200' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 302!!!!");   
+        $this->assertTrue($client->getResponse()->getStatusCode() == '302' ,
+                "Response code is: ".$client->getResponse()->getStatusCode().
+                "not 302!!!!");   
+    }
+    
+    
 }
